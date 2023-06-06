b5af445eb8d84220ad4f855519e1f9cc9041f5a4
dimanranawaka <dimanranawaka@gmail.com>
2023-06-06 12:54:44 +0530

    Adding ClientAppInitializer and ServerAppInitializer files

--- ChatApp_JavaSocket/src/ClientAppInitializer.java
+++ ChatApp_JavaSocket/src/ClientAppInitializer.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ClientAppInitializer extends Application {
 
@@ -8,7 +13,12 @@ public class ClientAppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("com/PlayTechPvtLtd/LiveChatApplication/view/InboxManager.fxml"));
+        primaryStage.setTitle("DreedConnect");
+        /*primaryStage.centerOnScreen();*/
+        primaryStage.setScene(new Scene(load));
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
