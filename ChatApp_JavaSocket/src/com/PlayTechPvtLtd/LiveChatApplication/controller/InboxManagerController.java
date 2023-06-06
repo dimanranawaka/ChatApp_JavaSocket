cb391786883f17de7b480e4e910ab6cade59616d
dimanranawaka <dimanranawaka@gmail.com>
2023-06-06 11:36:00 +0530

    Configure InboxManagerController and InboxManager.fxml files

--- ChatApp_JavaSocket/src/com/PlayTechPvtLtd/LiveChatApplication/controller/InboxManagerController.java
+++ ChatApp_JavaSocket/src/com/PlayTechPvtLtd/LiveChatApplication/controller/InboxManagerController.java
@@ -1,4 +1,51 @@
 package com.PlayTechPvtLtd.LiveChatApplication.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
 public class InboxManagerController {
+    public Pane profile;
+    public Label fullName;
+    public Label email;
+    public Label phoneNo;
+    public Label gender;
+    public TextField fileChoosePath;
+    public ImageView proImage;
+    public TextArea msgRoom;
+    public Pane chat;
+    public TextField msgField;
+    public ImageView emojiBtn;
+    public ImageView imageBtn;
+    public Label clientName;
+    public Button profileBtn;
+    public Circle showProPic;
+
+    public void handleProfileBtn(ActionEvent actionEvent) {
+    }
+
+    public void imageBtnOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void emojiBtnOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void sendMessageByKey(KeyEvent keyEvent) {
+    }
+
+    public void handleSendEvent(MouseEvent mouseEvent) {
+    }
+
+    public void saveImage(ActionEvent actionEvent) {
+    }
+
+    public void chooseImageButton(ActionEvent actionEvent) {
+    }
 }
