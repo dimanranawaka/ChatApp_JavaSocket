25ccaa9d3082adda5d46ffc239465e72913f0297
dimanranawaka <dimanranawaka@gmail.com>
2023-06-06 10:44:23 +0530

    Configure CreateNewUserAccount.fxml file

--- ChatApp_JavaSocket/src/com/PlayTechPvtLtd/LiveChatApplication/controller/CreateNewUserAccountController.java
+++ ChatApp_JavaSocket/src/com/PlayTechPvtLtd/LiveChatApplication/controller/CreateNewUserAccountController.java
@@ -1,4 +1,43 @@
 package com.PlayTechPvtLtd.LiveChatApplication.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
 public class CreateNewUserAccountController {
+    public Button btnSignUp;
+    public PasswordField passWord;
+    public TextField userName;
+    public TextField regPhoneNo;
+    public Label checkEmail;
+    public Label nameExists;
+    public RadioButton female;
+    public ToggleGroup Gender;
+    public RadioButton male;
+    public TextField regName;
+    public Label goBack;
+    public Label success;
+    public Label controlRegLabel;
+    public Button getStarted;
+    public TextField regEmail;
+    public TextField regFirstName;
+    public PasswordField regPass;
+    public ImageView btnBack;
+    public Pane pnSignUp;
+    public Label loginNotifier;
+    public Pane pnSignIn;
+
+    public void handleMouseEvent(MouseEvent mouseEvent) {
+    }
+
+    public void registration(ActionEvent actionEvent) {
+    }
+
+    public void handleButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void login(ActionEvent actionEvent) {
+    }
 }
